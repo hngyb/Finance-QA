2b01d5dccb0872729d07152b0ec3277ccb8f00e9
yusunk <bangmir27@gmail.com>
2021-06-29 04:59:55 +0900

    revised
    - compare.py 생성 (company.py, report.py 수정 포함)
    - view.py 수정 (report.py 수정 포함)

--- app/view.py
+++ app/view.py
@@ -6,6 +6,7 @@ import time
 
 from app.database.conn import db
 import app.database.company as company
+import app.database.report as report
 
 import requests, json
 
@@ -13,8 +14,8 @@ import requests, json
 class View:
     def __init__(self): # 디비 접근 분리 필요
         company_dict = {"company_name":[], "stock_code":[]}
-        sess = next(db.session())
-        for data in company.get_all_companies(sess):
+        self.sess = next(db.session())
+        for data in company.get_all_companies(self.sess):
             company_dict[data.company_name] = data.stock_code
     
         self.companies = company_dict
@@ -36,41 +37,55 @@ class View:
         
         return res
 
+    def get_reportinfo(self, name):
+        info = {}
+        stock_code = self.companies[name]
+        info['title'] = report.get_title(self.sess, stock_code)[0]
+        info['report_date'] = report.get_recent_date(self.sess, stock_code)[0]
+        info['source'] = report.get_source(self.sess, stock_code)[0]
+        info['url'] = report.get_url(self.sess, stock_code)[0]
+        return info
+
     def show_processbar(self):
-        put_processbar('bar', auto_close = True);
+        put_processbar('bar', auto_close = True)
         for i in range(1, 11):
             set_processbar('bar', i / 10)
             time.sleep(0.1)
 
     def webio(self):
         more = True
-        cnt = 1
         
         while(more):
             # input
-            input_qa = input_group('쏟아지는 증권사 리포트😱, 궁금한 정보만 찾고싶다면?', [
+            input_qa = input_group('쏟아지는 증권사 리포트, 궁금한 정보만 찾고싶다면?', [
                 input('📈종목명을 입력해주세요.', name = 'name', type = TEXT, datalist = list(self.companies.keys()), required = True),
-                input('🧐무엇이 궁금하신가요?', name = 'question', placeholder = '현재 목표주가는 얼마인가요?', required = True)
+                input('🧐무엇이 궁금하신가요?', name = 'question', placeholder = 'EX) 현재 목표주가는 얼마인가요?', required = True)
             ], validate = self.check_name)
-            paragraph = '이 보고서는 SK증권의 보고서입니다.'   
         
             # loading
+            answer = self.get_answer(input_qa['name'], input_qa['question'])
+            reportinfo = self.get_reportinfo(input_qa['name'])
             self.show_processbar()
         
             # output
-            put_markdown('# Result %s' %cnt)
+            put_markdown('# Result [%s]' %input_qa["name"])
 
             put_tabs([
                 {'title': 'Question & Answer', 'content': [
                     put_markdown('### 🧐Question'), 
                     put_code('%s' %input_qa['question']),
                     put_markdown('### 🤗Answer'),
-                    put_code('%s' %self.get_answer(input_qa['name'], input_qa['question']))
+                    put_code('%s' %answer)
                 ]},
-                {'title': 'Appendix', 'content': paragraph}
+                {'title': 'Appendix', 'content': put_table([
+                                                    ['제목', reportinfo['title']],
+                                                    ['날짜', reportinfo['report_date']],
+                                                    ['출처', reportinfo['source']],
+                                                    ['원문 리포트', put_markdown('[Link](%s)' %reportinfo["url"])]
+                                                ])
+                }
             ])
-            
+
             more = actions(label = "더 궁금한 점이 있으신가요?",
                             buttons =[{'label': '네, 더 질문할래요!','value': True},
                                     {'label':'아니요, 다음에 이용할게요.', 'value' : False}])
-            cnt += 1
